b370dfcef682c1d00dbfef7d129d64e5adf15479
Alexandrina Sobol <alexandrinasobol11@gmail.com>
2023-02-09 19:49:29 +0200

    [add] url to authentication, delete top bar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from "react-native";
+
+const Home = () => {
+
+    return (
+        <SafeAreaView>
+            <StatusBar hidden={true}/>
+            <Text>Hello</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Home;
---
+++ src/pages/authentication/constants.ts
@@ -0,0 +1,8 @@
+export const VIEW_LOGIN_ID = "username"
+export const VIEW_PASS_ID = "password"
+export const VIEW_OTP_ID = "otp-input"
+export const VIEW_SET_OTP_INPUT_ID = "totp"
+export const VIEW_SET_OTP_FORM_ID = "kc-totp-settings-form"
+export const VIEW_OTP_QR_CODE_ID = "kc-totp-secret-qr-code"
+export const VIEW_OTP_CONFIG_ID = "totp"
+export const VIEW_LOGIN_BTN_ID = "kc-login"
