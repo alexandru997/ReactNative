dd11b88321ee36f321ad40b0ef56e9d859887100
Alexandrina Sobol <alexandrinasobol11@gmail.com>
2023-02-10 17:51:04 +0200

    [add] splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Authentication from "./src/pages/authentication/Authentication";
+import Home from "./src/pages/Home";
 
 function App(): JSX.Element {
+    // const [showWebView, setShowWebView] = useState<boolean>(true);
+    //
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setShowWebView(false)
+    //     }, 20000);
+    //     return () => clearTimeout(timer);
+    // }, []);
 
     return (
-        <Authentication />
+        <>
+            <Authentication/>
+            {/*{showWebView*/}
+            {/*    ? <Authentication/>*/}
+            {/*    : <Home/>*/}
+            {/*}*/}
+        </>
     );
 }
 
